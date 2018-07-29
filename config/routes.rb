24a0295a3d28ff7d815183b12b3f23f845196b15
EC2 Default User <ec2-user@ip-172-31-92-1.ec2.internal>
2018-07-29 03:54:46 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
+
+  get 'microposts/destroy'
+
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
@@ -7,4 +11,6 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+
+  resources :microposts, only: [:create, :destroy]
 end
